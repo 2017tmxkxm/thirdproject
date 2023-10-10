baa5540415e6b02d2ca724e565ef9333f81725e0
2017tmxkxm <2017tmxkxm@gmail.com>
2023-10-10 16:12:57 +0900

    OAuth 로그인 추가

--- thirdproject/src/main/java/com/example/thirdproject/config/SecurityConfig.java
+++ thirdproject/src/main/java/com/example/thirdproject/config/SecurityConfig.java
@@ -2,26 +2,33 @@ package com.example.thirdproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)  // Secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests()
+        http.authorizeHttpRequests()
                 .antMatchers("/user/**").authenticated()
-                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").hasRole("ADMIN")  // ROLE_ 기본적으로 추가
+                .antMatchers("/manager/**").hasAnyRole("ADMIN, MANAGER")    // ROLE_ 기본적으로 추가, role들 중 하나라도 가지고 있으면 true 반환
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm");   // 구글 로그인이 완료된 뒤의 후처리가 필요하다.
         return http.build();
     }
 
