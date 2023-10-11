3d6c6fcd5d1bfe405f6b4a3ddfefe629e0f6e8fa
2017tmxkxm <2017tmxkxm@gmail.com>
2023-10-11 15:49:51 +0900

    일반로그인, OAuth 로그인 공통으로 사용할 수 있게 implements에 OAuth2User추가

--- thirdproject/src/main/java/com/example/thirdproject/config/auth/PrincipalDetails.java
+++ thirdproject/src/main/java/com/example/thirdproject/config/auth/PrincipalDetails.java
@@ -1,21 +1,33 @@
 package com.example.thirdproject.config.auth;
 
 import com.example.thirdproject.model.User;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
+@Data
 @Slf4j
-public class PrincipalDetails implements UserDetails {
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;
+    private Map<String, Object> attributes;
 
+    // 일반 로그인
     public PrincipalDetails(User user) {
         this.user = user;
+    }
+
+    // OAuth 로그인
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
     }
 
     @Override
@@ -60,4 +72,14 @@ public class PrincipalDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+    @Override
+    public String getName() {
+        return null;
+    }
+
 }
