c90d0e062e942b05270b637d777d943cb3b8a3fe
2017tmxkxm <2017tmxkxm@gmail.com>
2023-10-11 15:50:54 +0900

    일반로그인, OAuth 로그인 정보 확인 Test 코드 추가

--- thirdproject/src/main/java/com/example/thirdproject/controller/IndexController.java
+++ thirdproject/src/main/java/com/example/thirdproject/controller/IndexController.java
@@ -1,17 +1,24 @@
 package com.example.thirdproject.controller;
 
+import com.example.thirdproject.config.auth.PrincipalDetails;
 import com.example.thirdproject.model.User;
 import com.example.thirdproject.repository.UserRepository;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -20,6 +27,26 @@ public class IndexController {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/test/login")
+    public @ResponseBody String testLogin(Authentication authentication, @AuthenticationPrincipal PrincipalDetails userDetails) {
+        log.info("authentication.getPrincipal() : {}", authentication.getPrincipal());
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        log.info("principalDetails.getUser() : {}", principalDetails.getUser());
+        log.info("userDetails.getUser() : {}", userDetails.getUser());
+        return "세션 정보 확인하기";
+    }
+
+    @GetMapping("/test/oauth/login")
+    public @ResponseBody String testOAuthLogin(Authentication authentication, @AuthenticationPrincipal OAuth2User oauth) {
+        log.info("authentication.getPrincipal() : {}", authentication.getPrincipal());
+
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        log.info("oAuth2User.getAttributes() : {}", oAuth2User.getAttributes());
+
+        log.info("OAuth2User : {}", oauth.getAttributes());
+        return "OAuth 세션 정보 확인하기";
+    }
 
     @GetMapping({"", "/"})
     public String index() {
