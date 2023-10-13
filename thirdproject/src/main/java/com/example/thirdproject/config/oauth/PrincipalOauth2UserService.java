cbf2be18b9c3c499803e192e3fe41cf3ce83b86d
2017tmxkxm <2017tmxkxm@gmail.com>
2023-10-13 14:12:02 +0900

    BCryptPasswordEncoder 추가 및 회원가입 강제 진행 코드 추가

--- thirdproject/src/main/java/com/example/thirdproject/config/oauth/PrincipalOauth2UserService.java
+++ thirdproject/src/main/java/com/example/thirdproject/config/oauth/PrincipalOauth2UserService.java
@@ -1,6 +1,12 @@
 package com.example.thirdproject.config.oauth;
 
+import com.example.thirdproject.config.auth.PrincipalDetails;
+import com.example.thirdproject.model.User;
+import com.example.thirdproject.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -11,14 +17,38 @@ import org.springframework.stereotype.Service;
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        log.info("getClientRegistration : {}", userRequest.getClientRegistration());
-        log.info("getAccessToken : {}", userRequest.getAccessToken());
-        log.info("getAccessToken().getTokenValue() : {}", userRequest.getAccessToken().getTokenValue());
-        log.info("getAttributes : {}", super.loadUser(userRequest).getAttributes());
-
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        return super.loadUser(userRequest);
+
+        // 회원가입을 강제로 진행
+        String provider = userRequest.getClientRegistration().getClientId();    // google
+        String providerId = oAuth2User.getAttribute("sub");
+        String username = provider +"_" + providerId;
+        String password = bCryptPasswordEncoder.encode("겟인데어");
+        String email = oAuth2User.getAttribute("email");
+        String role = "ROLE_USER";
+
+        User userEntity = userRepository.findByUsername(username);
+
+        if(userEntity == null) {
+            userEntity = User.builder()
+                    .username(username)
+                    .password(password)
+                    .email(email)
+                    .role(role)
+                    .provider(provider)
+                    .providerId(providerId)
+                    .build();
+            userRepository.save(userEntity);
+        }
+
+        return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
     }
 }
